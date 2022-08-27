3b1e60ef4b59a1f656ab57f2a695ffed24c751e0
Aryel Anne Louise de Souza Gabriel <69636549+Aryel15@users.noreply.github.com>
2022-08-27 13:21:45 -0300

    Mudanças no style

--- src/js/fonte.js
+++ src/js/fonte.js
@@ -13,3 +13,18 @@ $(document).ready(function(){
         }
     })
 })
+$(document).ready(function(){
+    let fonte = 16
+    $('#aaumentar').click(function(){
+        if(fonte<22){
+            fonte++
+            $('body').css({'font-size': fonte+'px'})
+        }
+    })
+    $('#ddiminuir').click(function(){
+        if(fonte>13){
+            fonte--
+            $('body').css({'font-size': fonte+'px'})
+        }
+    })
+})
